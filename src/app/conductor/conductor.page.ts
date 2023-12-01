a2cc37cccbafd20933761bbeec7bdcea8bf96a84
alex <ale.roa@duocuc.cl>
2023-12-01 16:30:28 -0300

    commitDB

--- src/app/conductor/conductor.page.ts
+++ src/app/conductor/conductor.page.ts
@@ -1,8 +1,7 @@
-// conductor.page.ts
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ViajeService } from '../services/viaje.service';
+
 @Component({
   selector: 'app-conductor',
   templateUrl: './conductor.page.html',
@@ -29,12 +28,36 @@ export class ConductorPage implements OnInit {
   }
 
   agregarViaje() {
-    this.viajeService.agregarViaje(this.nuevoViaje);
-    this.nuevoViaje = {
-      lugarInicio: '',
-      lugarFinal: '',
-      asientosDisponibles: 0,
-      hora: '',
-    };
+    // Validar el formulario antes de enviarlo
+    if (this.validarFormulario()) {
+      this.viajeService.agregarViaje(this.nuevoViaje)
+        .then(() => {
+          // Limpiar el formulario después de agregar el viaje
+          this.nuevoViaje = {
+            lugarInicio: '',
+            lugarFinal: '',
+            asientosDisponibles: 0,
+            hora: '',
+          };
+  
+          console.log('Viaje agregado exitosamente.');
+        })
+        .catch(error => {
+          console.error('Error al agregar viaje', error);
+        });
+    } else {
+      console.error('El formulario no es válido. Por favor, complete todos los campos.');
+    }
+  }
+  
+
+  private validarFormulario(): boolean {
+    // Implementa la lógica de validación según tus necesidades
+    return (
+      this.nuevoViaje.lugarInicio.trim() !== '' &&
+      this.nuevoViaje.lugarFinal.trim() !== '' &&
+      this.nuevoViaje.asientosDisponibles > 0 &&
+      this.nuevoViaje.hora.trim() !== ''
+    );
   }
 }
--- src/app/services/viaje.service.ts
+++ src/app/services/viaje.service.ts
@@ -1,81 +1,94 @@
+// viaje.service.ts
+
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ViajeService {
-  viajes: any[] = []; // Declarar la propiedad viajes
   constructor(private sqlite: SQLite) {
-    /*this.crearTablaViajes();*/
+    this.crearTablaViajes();
   }
 
-  private crearTablaViajes() {
-    this.sqlite.create({
+  private async crearTablaViajes() {
+    try {
+      const db = await this.sqlite.create({
+        name: 'DBMovil.db',
+        location: 'default'
+      });
+  
+      await db.executeSql('CREATE TABLE IF NOT EXISTS viajes (id INTEGER PRIMARY KEY AUTOINCREMENT, lugarInicio TEXT, lugarFinal TEXT, asientosDisponibles INTEGER, hora TEXT)', [])
+        .then(() => console.log('Tabla viajes creada con éxito'))
+        .catch(error => console.error('Error al crear tabla viajes', error));
+    } catch (error) {
+      console.error('Error al abrir la base de datos', error);
+    }
+  }
+  
+
+  async agregarViaje(nuevoViaje: any) {
+    try {
+      const db = await this.sqlite.create({
+        name: 'DBMovil.db',
+        location: 'default'
+      });
+  
+      await db.executeSql('INSERT INTO viajes (lugarInicio, lugarFinal, asientosDisponibles, hora) VALUES (?, ?, ?, ?)',
+        [nuevoViaje.lugarInicio, nuevoViaje.lugarFinal, nuevoViaje.asientosDisponibles, nuevoViaje.hora]);
+  
+      console.log('Viaje agregado con éxito:', nuevoViaje);
+    } catch (error) {
+      console.error('Error al agregar viaje:', error);
+    }
+  }
+  
+
+
+  async modificarViaje(id: number, viaje: any) {
+    const db = await this.sqlite.create({
       name: 'DBMovil.db',
       location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('CREATE TABLE IF NOT EXISTS viajes (id INTEGER PRIMARY KEY AUTOINCREMENT, lugarInicio TEXT, lugarFinal TEXT, asientosDisponibles INTEGER, hora TEXT)', [])
-        .then(() => console.log('Tabla viajes creada'))
-        .catch(error => console.error('Error al crear tabla viajes', error));
-    }).catch(error => console.error('Error al abrir la base de datos', error));
+    });
+
+    await db.executeSql('UPDATE viajes SET lugarInicio=?, lugarFinal=?, asientosDisponibles=?, hora=? WHERE id=?',
+      [viaje.lugarInicio, viaje.lugarFinal, viaje.asientosDisponibles, viaje.hora, id])
+      .then(() => console.log('Viaje modificado'))
+      .catch(error => console.error('Error al modificar viaje', error));
   }
 
-  agregarViaje(nuevoViaje: any) {
-    this.sqlite.create({
-      name: 'tu_base_de_datos.db',
+  async listarViajes() {
+    const db = await this.sqlite.create({
+      name: 'DBMovil.db',
       location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO viajes (lugarInicio, lugarFinal, asientosDisponibles, hora) VALUES (?, ?, ?, ?)',
-        [nuevoViaje.lugarInicio, nuevoViaje.lugarFinal, nuevoViaje.asientosDisponibles, nuevoViaje.hora])
-        .then(() => console.log('Viaje agregado'))
-        .catch(error => console.error('Error al agregar viaje', error));
-    }).catch(error => console.error('Error al abrir la base de datos', error));
+    });
+
+    return db.executeSql('SELECT * FROM viajes', [])
+      .then(data => {
+        let viajes = [];
+        for (let i = 0; i < data.rows.length; i++) {
+          viajes.push(data.rows.item(i));
+        }
+        return viajes;
+      })
+      .catch(error => console.error('Error al listar viajes', error));
   }
 
-  modificarViaje(id: number, viaje: any) {
-    this.sqlite.create({
-      name: 'tu_base_de_datos.db',
+  async elegirViaje(id: number) {
+    const db = await this.sqlite.create({
+      name: 'DBMovil.db',
       location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('UPDATE viajes SET lugarInicio=?, lugarFinal=?, asientosDisponibles=?, hora=? WHERE id=?',
-        [viaje.lugarInicio, viaje.lugarFinal, viaje.asientosDisponibles, viaje.hora, id])
-        .then(() => console.log('Viaje modificado'))
-        .catch(error => console.error('Error al modificar viaje', error));
-    }).catch(error => console.error('Error al abrir la base de datos', error));
-  }
+    });
 
-  listarViajes() {
-    return this.sqlite.create({
-      name: 'tu_base_de_datos.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      return db.executeSql('SELECT * FROM viajes', [])
-        .then(data => {
-          let viajes = [];
-          for (let i = 0; i < data.rows.length; i++) {
-            viajes.push(data.rows.item(i));
-          }
-          return viajes;
-        })
-        .catch(error => console.error('Error al listar viajes', error));
-    }).catch(error => console.error('Error al abrir la base de datos', error));
-  }
-
-  elegirViaje(id: number) {
-    return this.sqlite.create({
-      name: 'tu_base_de_datos.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      return db.executeSql('SELECT * FROM viajes WHERE id=?', [id])
-        .then(data => {
-          if (data.rows.length > 0) {
-            return data.rows.item(0);
-          } else {
-            return null;
-          }
-        })
-        .catch(error => console.error('Error al elegir viaje', error));
-    }).catch(error => console.error('Error al abrir la base de datos', error));
+    return db.executeSql('SELECT * FROM viajes WHERE id=?', [id])
+      .then(data => {
+        if (data.rows.length > 0) {
+          return data.rows.item(0);
+        } else {
+          return null;
+        }
+      })
+      .catch(error => console.error('Error al elegir viaje', error));
   }
 
   tomarAsiento(idViaje: number, asiento: number) {
